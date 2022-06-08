16b14af788539b1481c93454a0ef4cd01551cb5f
Jérémy Belleau <71091127+Belleauu@users.noreply.github.com>
2022-06-08 12:54:51 -0400

    removed unecessary js and started date time picker

--- booking/scheduling/main.js
+++ booking/scheduling/main.js
@@ -1,13 +1,7 @@
-var storedName = localStorage.getItem("inputName");
-var storedLastName = localStorage.getItem("inputLastName");
-var storedEmail = localStorage.getItem("inputEmail");
-var storedTelephone = localStorage.getItem("inputTelephone");
-
-var storedCheckDentures = localStorage.getItem("inputCheckDentures");
-var storedCheckHygiene = localStorage.getItem("inputCheckHygiene");
-var storedCheckInvisalign = localStorage.getItem("inputCheckInvisalign");
-var storedCheckRoot = localStorage.getItem("inputCheckRoot");
-var storedCheckWhitening = localStorage.getItem("inputCheckWhitening");
-var storedCheckExtraction = localStorage.getItem("inputCheckExtraction");
-
-document.getElementById()
+$(".dropdown-menu li a").click(function(){
+    var selectedProfessional = $(this).attr('data-value');
+    if (!(selectedProfessional === "")){
+        document.getElementById("titleProfessional").innerHTML = selectedProfessional;
+        document.getElementById("hiddenTitle").className = "d-block";
+    }
+});
