23c53ff704b36c1fd1ee8a2f7795dc18058f4143
Jérémy Belleau <jbell163@uottawa.ca>
2022-06-07 20:16:47 -0400

    introducing form variables for booking

---
+++ booking/scheduling/main.js
@@ -0,0 +1,11 @@
+var storedName = localStorage.getItem("inputName");
+var storedLastName = localStorage.getItem("inputLastName");
+var storedEmail = localStorage.getItem("inputEmail");
+var storedTelephone = localStorage.getItem("inputTelephone");
+
+var storedCheckDentures = localStorage.getItem("inputCheckDentures");
+var storedCheckHygiene = localStorage.getItem("inputCheckHygiene");
+var storedCheckInvisalign = localStorage.getItem("inputCheckInvisalign");
+var storedCheckRoot = localStorage.getItem("inputCheckRoot");
+var storedCheckWhitening = localStorage.getItem("inputCheckWhitening");
+var storedCheckExtraction = localStorage.getItem("inputCheckExtraction");
