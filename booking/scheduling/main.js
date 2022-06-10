0fe5f922d45bb304f204bd3776a81900cd287c0e
Jérémy Belleau <71091127+Belleauu@users.noreply.github.com>
2022-06-09 22:01:01 -0400

    fixed links and colors

--- booking/scheduling/main.js
+++ booking/scheduling/main.js
@@ -1,23 +1,4 @@
-$(".dropdown-menu li a").click(function(){
-    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-  });
-
-
-// Linked date and time picker 
-	// start date date and time picker 
-	$('#datepicker-start').datetimepicker();
-
-	// End date date and time picker 
-	$('#datepicker-end').datetimepicker({
-		useCurrent: false 
-	});
-	
-	// start date picke on chagne event [select minimun date for end date datepicker]
-	$("#datepicker-start").on("dp.change", function (e) {
-		$('#datepicker-end').data("DateTimePicker").minDate(e.date);
-	});
-	// Start date picke on chagne event [select maxmimum date for start date datepicker]
-	$("#datepicker-end").on("dp.change", function (e) {
-		$('#datepicker-start').data("DateTimePicker").maxDate(e.date);
-	});
+$(".dropdown-menu li a").click(function () {
+	$(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+	$(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+});
