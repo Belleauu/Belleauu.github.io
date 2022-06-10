73dd05ba6585af4fa18ec763523fb6a86ff57d75
Jérémy Belleau <jbell163@uottawa.ca>
2022-06-09 20:40:56 -0400

    Adjusting some links

--- booking/scheduling/main.js
+++ booking/scheduling/main.js
@@ -1,7 +1,7 @@
-$(".dropdown-menu li a").click(function(){
-    var selectedProfessional = $(this).attr('data-value');
-    if (!(selectedProfessional === "")){
-        document.getElementById("titleProfessional").innerHTML = selectedProfessional;
-        document.getElementById("hiddenTitle").className = "d-block";
-    }
-});
+// $(".dropdown-menu li a").click(function(){
+//     var selectedProfessional = $(this).attr('data-value');
+//     if (!(selectedProfessional === "")){
+//         document.getElementById("titleProfessional").innerHTML = selectedProfessional;
+//         document.getElementById("hiddenTitle").className = "d-block";
+//     }
+// });
